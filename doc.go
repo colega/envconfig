df1f3f22d8a4721a4f801db6e0aa2c5858b9856f
Oleg Zaytsev <mail@olegzaytsev.com>
2020-04-27 12:30:28 +0200

    Add copyright notice

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
 // Copyright (c) 2013 Kelsey Hightower. All rights reserved.
+// Copyright (c) 2020 Oleg Zaytsev. All rights reserved.
+//
 // Use of this source code is governed by the MIT License that can be found in
 // the LICENSE file.
 
